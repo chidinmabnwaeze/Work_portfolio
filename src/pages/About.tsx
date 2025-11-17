92e24d20dda2cedd6d9b7ca06b4398ba74e18c8a
Chidinma Nwaeze <chidinmabnwaeze@gmail.com>
2025-11-17 16:52:59 +0100

    adjusted about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import aboutimg from "../assets/OQCI8I0.jpg";
+import aboutimg from "../assets/WhatsApp Image 2025-11-17 at 13.03.57_b6a1b2a0.jpg";
 import { Download, Briefcase } from "lucide-react";
 
 type Metrics = {
@@ -14,21 +14,18 @@ const About = () => {
   return (
     <main className="flex flex-col justify-center items-center m-15">
       <p>Get to know me</p>
-      <hr/>
+      <hr />
       <h1 className="font-bold text-4xl">About Me</h1>
       <div className="flex gap-10 my-10">
-        <section>
-          <img src={aboutimg} alt="Chidinma" />
+        <section className=" w-full">
+          <img src={aboutimg} alt="Chidinma" className="rounded-md" />
         </section>
-        <section className="mt-20">
+        <section className="mt-20 ml-30 mr-">
           <span className="flex gap-3 text-xl">
-            <blockquote className="text-fuchsia-950 text-4xl italic">
-              Chidinma
-            </blockquote>
-            is a Frontend Developer with a passion for creating beautiful and
-            functional web applications. With a strong foundation in HTML, CSS,
-            and JavaScript, she brings designs to life with precision and
-            creativity.
+           <span className=" chidinma text-fuchsia-950 text-3xl">Chidinma </span> is a Frontend Developer with a passion for creating
+            beautiful and functional web applications. With a strong foundation
+            in HTML, CSS, and JavaScript, she brings designs to life with
+            precision and creativity.
             <br />
             for creating beautiful and functional web applications. With a
             strong foundation in HTML, CSS, and JavaScript, she brings designs
@@ -50,7 +47,7 @@ const About = () => {
               <Briefcase />
             </button>
 
-            <button className="border-2 border-purple-600 text-purple-600 p-4 rounded flex items-center gap-2 hover:">
+            <button className="border-2 border-purple-600 text-purple-600 p-4 rounded flex items-center gap-2 hover:bg-fuchsia-950 transition transform hover:scale-105 hover:text-white">
               Download CV
               <Download />
             </button>
