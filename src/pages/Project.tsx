6de9cb2b596ff022f97d006a538e746b5b162ff9
Chidinma Nwaeze <chidinmabnwaeze@gmail.com>
2025-11-26 13:45:39 +0100

    project 1 and skills

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,10 +1,19 @@
 import img1 from "../assets/images/IMG_1134.JPG"
+import reactLogo from "../assets/logo/React-icon.svg.png"
+import jsLogo from "../assets/logo/javascript-logo-png_seeklogo-255387.png"
+import viteLogo from "../assets/logo/Vitejs-logo.svg.png"
+
+type logoProps = {
+  src: string;
+  alt: string;
+}; 
 
 type ProjectProps = {
   id: number;
   title: string;
   description: string;
   imageUrl: string;
+  logo: logoProps[];
 };
 
 const projects: ProjectProps[] = [
@@ -13,10 +22,24 @@ const projects: ProjectProps[] = [
     title: "ClockIT Attendance System",
     description:
       "ClockIT is an Attendance Clocking System that tracks employees’ daily attendance, displays a real-time clock, and summarizes all employees with their clock-in times. Employees can also request leave directly from the platform, and it even calculates payroll based on attendance.",
-    imageUrl: "../assets/images/IMG_1134.JPG",
+    logo: [
+      {
+        src: jsLogo,
+        alt: "JavaScript Logo",
+      },
+      {
+        src: reactLogo,
+        alt: "React Logo",
+      },
+         {
+        src: viteLogo,
+        alt: "vite Logo",
+      },
+    ],
+    imageUrl: "",
   },
 ];
-const Project = ({ id, title, description, imageUrl }: ProjectProps) => {
+const Project = () => {
   return (
     <main className="flex flex-col justify-center items-center m-15 bg-black p-20 text-white">
       <p>Here are some of my projects</p>
@@ -30,14 +53,25 @@ const Project = ({ id, title, description, imageUrl }: ProjectProps) => {
               key={index}
             >
               <div className="m-8 flex flex-col justify-center">
-              <p className="bg-fuchsia-950 rounded-full w-16 h-16 text-center p-5 text-white">{project.id}</p>
-              <h1 className="text-2xl py-5">{project.title}</h1>
+                <p className="bg-fuchsia-950 rounded-full w-16 h-16 text-center p-5 text-white">
+                  {project.id}
+                </p>
+                <h1 className="text-2xl py-5">{project.title}</h1>
                 <p>{project.description}</p>
-                <button className="p-4 my-6 max-w-2xs text-white rounded bg-fuchsia-950 hover:bg-p">View Project</button>
+               <ul>
+                  {project.logo.map((logoItem, logoIndex) => (
+                    <li key={logoIndex} className="inline-block mr-2 my-3">
+                      <img  src={logoItem.src} alt={logoItem.alt} className="w-8 h-8" />
+                    </li>
+                  ))}
+               </ul>
+                <button className="p-4 my-6 max-w-2xs text-white rounded bg-fuchsia-950 hover:bg-white hover:font-bold hover:text-fuchsia-950 hover:transition-all border-2 border-fuchsia-950">
+                  View Project
+                </button>
               </div>
 
               <div className="max-w-200">
-                <img src={img1}alt="Project image" />
+                <img src={img1} alt="Project image" />
               </div>
             </section>
           ))}
