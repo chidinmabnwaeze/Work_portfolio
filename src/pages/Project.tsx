bc5825eb6d45bcd6ef1b98f6be4554d8107f12f2
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2026-01-26 22:20:06 +0100

    raised project card

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,21 +1,28 @@
-import project1 from "../assets/Screenshot 2026-01-22 215413.png"
-import project2 from "../assets/Screenshot 2026-01-22 215432.png"
+import project1 from "../assets/Screenshot 2026-01-22 215413.png";
+import project2 from "../assets/Screenshot 2026-01-22 215432.png";
+import next from "../assets/logo/next js.jpg";
 
 export default function Project() {
   const projects = [
     {
       img: project1,
-      name: "Eduprotos",
-      description: "Lorem ipsum",
+      name: "EDUPROTOS",
+      description:
+        "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+      skills: [
+        {
+          skill: next,
+        },
+      ],
     },
-     {
+    {
       img: project2,
-      name: "Eduprotos",
+      name: "EDUPROTOS",
       description: "",
     },
-      {
+    {
       img: "",
-      name: "Eduprotos",
+      name: "EDUPROTOS",
       description: "",
     },
   ];
@@ -27,12 +34,22 @@ export default function Project() {
 
       <section className="grid grid-cols-2 p-20 m-5 border-stone-800 bg-black ">
         {projects.map((project, index) => (
-          <div className="p_card bg-linear-90 bg-white p-10 m-2 rounded-2xl hover:transform hover:translate-y-3 hover:transition" key={index}>
-            <img src={project.img} alt="project 1" />
-            <div className="border-t border-t-gray-200 m-8 py-3 ">
-            <h2 className="text-2xl my-2 ">{project.name}</h2>
-            <p>{project.description}</p>
-            <button className="bg-fuchsia-950 text-white p-3 mt-5 rounded">View Project</button>
+          <div
+            className="p_card bg-linear-90 bg-white p-10 m-2 rounded-2xl hover:transform hover:translate-y-3 hover:transition"
+            key={index}
+          >
+            <img
+              src={project.img}
+              alt="project 1"
+              className="bg-gray-300 hover:scale-3d transform"
+            />
+            <div className="m-8 py-3 ">
+              <h2 className="text-2xl my-2 ">{project.name}</h2>
+              <p>{project.description}</p>
+              <div>{project.skills?.skill}</div>
+              <button className="bg-fuchsia-950 text-white p-3 mt-5 rounded">
+                View Project
+              </button>
             </div>
           </div>
         ))}
