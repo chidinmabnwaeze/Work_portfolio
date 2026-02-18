b32bfbad393f73b01ce054b1a788ff5557ef55cc
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2026-02-18 01:20:53 +0100

    contact bg

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -9,45 +9,48 @@ export default function Contact() {
         backgroundImage: `url(${contactImg})`,
       }}
     >
-      <p className="mt-10">Get in Touch</p>
-      <h1 className="font-bold text-4xl">Contact</h1>
+      <div className="absolute inset-0 bg-[#352055]/70"></div>
+      <div className="relative z-10">
+        <p className="mt-10">Get in Touch</p>
+        <h1 className="font-bold text-4xl">Contact</h1>
 
-      <form
-        action=""
-        className="border border-gray-200 rounded-xl p-8 w-full max-w-2xl my-8"
-      >
-        <div className="flex flex-col gap-2 my-7">
-          <label htmlFor="name">Full Name</label>
-          <input
-            type="text"
-            placeholder="Enter your full name"
-            className="border border-gray-300 rounded p-4 focus-visible:bg-fuchsia-50 focus:border-fuchsia-200  focus:outline-0"
-          />
-        </div>
-        <div className="flex flex-col gap-2 my-7">
-          <label htmlFor="email">Email Address</label>
-          <input
-            type="text"
-            placeholder="Enter your email address"
-            className="border border-gray-300 rounded p-4 focus-visible:bg-fuchsia-50 focus:border-fuchsia-200  focus:outline-0"
-          />
-        </div>
-        <div className="flex flex-col gap-2 my-7">
-          <label htmlFor="name">Your Message</label>
-          <textarea
-            rows={4}
-            placeholder="Send me a message"
-            className="border border-gray-300 rounded p-4 focus-visible:bg-fuchsia-50 focus:border-fuchsia-200  focus:outline-0"
-          />
-        </div>
+        <form
+          action=""
+          className="border border-gray-200 rounded-xl p-8 w-full max-w-2xl my-8"
+        >
+          <div className="flex flex-col gap-2 my-7">
+            <label htmlFor="name">Full Name</label>
+            <input
+              type="text"
+              placeholder="Enter your full name"
+              className="border border-gray-300 rounded p-4 focus-visible:bg-fuchsia-50 focus:border-fuchsia-200  focus:outline-0"
+            />
+          </div>
+          <div className="flex flex-col gap-2 my-7">
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="text"
+              placeholder="Enter your email address"
+              className="border border-gray-300 rounded p-4 focus-visible:bg-fuchsia-50 focus:border-fuchsia-200  focus:outline-0"
+            />
+          </div>
+          <div className="flex flex-col gap-2 my-7">
+            <label htmlFor="name">Your Message</label>
+            <textarea
+              rows={4}
+              placeholder="Send me a message"
+              className="border border-gray-300 rounded p-4 focus-visible:bg-fuchsia-50 focus:border-fuchsia-200  focus:outline-0"
+            />
+          </div>
 
-        <button className="flex justify-center gap-2 text-white bg-fuchsia-950 p-4 rounded w-full text-center hover:bg-white hover:text-fuchsia-950 hover:border border-fuchsia-950 cursor-pointer">
-          Off to my Inbox
-          <span>
-            <ArrowUpRight width={20} />
-          </span>
-        </button>
-      </form>
+          <button className="flex justify-center gap-2 text-white bg-fuchsia-950 p-4 rounded w-full text-center hover:bg-white hover:text-fuchsia-950 hover:border border-fuchsia-950 cursor-pointer">
+            Off to my Inbox
+            <span>
+              <ArrowUpRight width={20} />
+            </span>
+          </button>
+        </form>
+      </div>
     </main>
   );
 }
